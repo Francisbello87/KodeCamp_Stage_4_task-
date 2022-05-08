8fd6520355118214ab362393219ff37e730a153f
Francis Bello <bellofrancis87@gmail.com>
2022-05-08 12:20:10 +0100

    Added Responsiveness

---
+++ main.js
@@ -0,0 +1,28 @@
+const mobileNav = document.querySelector(".mobile-nav");
+const menuIcon = document.querySelector(".menu-icon");
+const mobileMenuItems = document.querySelectorAll(".mobile-nav .menu-items li");
+
+menuIcon.addEventListener("click", () => {
+  mobileNav.classList.toggle("active");
+});
+mobileMenuItems.forEach((i) => {
+  i.addEventListener("click", () => {
+    mobileNav.classList.remove("active");
+  });
+});
+
+// FAQ SECTION
+var acc = document.getElementsByClassName("accordion");
+var i;
+
+for (i = 0; i < acc.length; i++) {
+  acc[i].addEventListener("click", function () {
+    this.classList.toggle("active");
+    var panel = this.nextElementSibling;
+    if (panel.style.maxHeight) {
+      panel.style.maxHeight = null;
+    } else {
+      panel.style.maxHeight = panel.scrollHeight + "px";
+    }
+  });
+}
